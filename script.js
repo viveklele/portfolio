67606748419168c8662c6c12799656549bf2646c
viveklele <viveklele123@gmail.com>
2024-09-07 18:46:14 +0530

    new brach added

---
+++ script.js
@@ -0,0 +1,9 @@
+function togglemenu(){
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
+
+
