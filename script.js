53b40535c972ce8d52f39b17531ab8782ba38fb4
viveklele <viveklele123@gmail.com>
2024-10-12 11:22:17 +0530

    changes adde

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-function togglemenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function togglemenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
 function linkdin() {
-    window.location.href = 'https://www.linkedin.com/in/your-profile-url';
+  window.location.href = 'https://www.linkedin.com/in/your-profile-url';
 }
+
 
 
 
